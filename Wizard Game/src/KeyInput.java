04ceec0c8161189504253d85dc068f0eb3f60421
mustufah711 <mustufah711@gmail.com>
2018-01-05 21:02:14 -0500

    fixed movement

--- Wizard Game/src/KeyInput.java
+++ Wizard Game/src/KeyInput.java
@@ -2,49 +2,43 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class KeyInput extends KeyAdapter{
+
+	Handler handler;
 	
-	Handler handler; //Do not initialize new instance since we need to access same linkedlist
-	
-	public KeyInput(Handler handler) {
+	public KeyInput(Handler handler){
 		this.handler = handler;
 	}
 	
-	public void keyPressed(KeyEvent e) {
+	public void keyPressed(KeyEvent e){
 		int key = e.getKeyCode();
-		for(int i =0; i<handler.object.size(); i++) {
-			GameObject tempObj = handler.object.get(i);
-			if(tempObj.getID() == ID.Player) {
-				if(key == KeyEvent.VK_W) { 
-					handler.setUp(true);
-					System.out.println("up pressed");
-				}
-				if(key == KeyEvent.VK_S) {
-					handler.setDown(true);
-					System.out.println("down pressed");
-				}
-				if(key == KeyEvent.VK_A) {
-					handler.setLeft(true);
-					System.out.println("left pressed");
-				}
-				if(key == KeyEvent.VK_D) {
-					handler.setRight(true);
-					System.out.println("right pressed");
-				}
+		
+		for(int i = 0; i < handler.object.size(); i++){
+			GameObject tempObject = handler.object.get(i);
+			
+			if(tempObject.getID() == ID.Player){
+				if(key == KeyEvent.VK_W) handler.setUp(true);
+				if(key == KeyEvent.VK_S) handler.setDown(true);
+				if(key == KeyEvent.VK_A) handler.setLeft(true);
+				if(key == KeyEvent.VK_D) handler.setRight(true);
 			}
 		}
+		
 	}
 	
-	public void keyRelased(KeyEvent e) {
+	public void keyReleased(KeyEvent e){
 		int key = e.getKeyCode();
-		for(int i =0; i<handler.object.size(); i++) {
-			GameObject tempObj = handler.object.get(i);
-			if(tempObj.getID() == ID.Player) {
-				System.out.println("hello");
-				if(key == KeyEvent.VK_UP) handler.setUp(false);
-				if(key == KeyEvent.VK_DOWN) handler.setDown(false);
-				if(key == KeyEvent.VK_LEFT) handler.setLeft(false);
-				if(key == KeyEvent.VK_RIGHT) handler.setRight(false);
+		
+		for(int i = 0; i < handler.object.size(); i++){
+			GameObject tempObject = handler.object.get(i);
+			
+			if(tempObject.getID() == ID.Player){
+				if(key == KeyEvent.VK_W) handler.setUp(false);
+				if(key == KeyEvent.VK_S) handler.setDown(false);
+				if(key == KeyEvent.VK_A) handler.setLeft(false);
+				if(key == KeyEvent.VK_D) handler.setRight(false);
 			}
-		}
+		}		
+		
+		
 	}
 }
